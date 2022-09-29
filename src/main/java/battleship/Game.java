c05d754837fc43494c6bdc78d7a26360910453ca
adrixo <adrianvalrom@gmail.com>
2022-09-29 15:26:25 +0200

    feat: add public Game interface

---
+++ src/main/java/battleship/Game.java
@@ -0,0 +1,25 @@
+package battleship;
+
+public class Game {
+
+    public void addPlayer() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void endTurn() {
+
+    }
+
+    public void print() {
+
+    }
+
+    public void fire() {
+
+    }
+
+}
